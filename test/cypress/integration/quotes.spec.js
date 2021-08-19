1b916d70ba674351380ca604a54a7dde18d196dd
Jovan Ilić <Jovan.Ilic@cloudflight.io>
2021-08-19 12:29:54 +0200

    Purposely fail the test

--- test/cypress/integration/quotes.spec.js
+++ test/cypress/integration/quotes.spec.js
@@ -1,6 +1,6 @@
 describe('Game of Quotes', () => {
     it('Open quotes site', () => {
         cy.visit('/')
-        cy.get('h5').contains('Game of Quotes')
+        cy.get('h5').contains('Game of sQuotes')
     })
 })
