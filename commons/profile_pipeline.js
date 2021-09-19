841043170a098ad586b04f390c2960290c64fabc
Joca <ilicjovan89@gmail.com>
2021-09-20 00:26:28 +0200

    feat(db.view): add all quotes view draft profile get pipeline

--- commons/profile_pipeline.js
+++ commons/profile_pipeline.js
@@ -1,18 +1,21 @@
 import db from './db.js';
 
 function get(client, user_id) {
+    const pipeline = [];
 
-    // TODO no more timeline
-    // let pipeline = [
-    //     { $match: { '_id': user_id } },
-    //     {
-    //         $project: {
-    //             username: 1, email: 1, timeline: 1,
-    //             solved: { $size: { $filter: { input: '$timeline', as: 'line', cond: { $eq: ['$$line.mark', 'quote'] } } } },
-    //             uploaded: { $size: { $filter: { input: '$timeline', as: 'line', cond: { $eq: ['$$line.mark', 'upload'] } } } }
-    //         }
-    //     }];
-    // return db.aggregate(client, pipeline);
+    const stage_1 = { $match: { '_id': user_id } };
+
+    const stage_2 = {
+        $project: {
+            username: 1, email: 1,
+            timeline: [{ insert_date: '2017-05-24 22:41:09', content: 'joca', addition: null, link: null, mark: 'user' }],
+            solved: { $size: '$solved' },
+            uploaded: { $size: '$uploaded' }
+        }
+    };
+
+    pipeline.push(stage_1, stage_2);
+    return db.aggregate(client, pipeline);
 }
 
 export default { get };
